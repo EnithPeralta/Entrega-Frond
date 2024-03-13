7bb16446591ee4aaa8e1f5f4d3aa0f9d521b17d6
Maria Enith Peralta <mosqueraperalta12@gmail.com>
2024-03-13 12:00:03 -0500

    cambio de la tabla

--- my-proyect/src/App.jsx
+++ my-proyect/src/App.jsx
@@ -10,7 +10,24 @@ function App() {
 
     {id: "1",nombre: "Maria",apellido: "Peralta",descripcion: "Locura total"},
     { id: "2", nombre: "Juan", apellido: "Angulo", descripcion: "Casa de campo" },
-    { id: "3", nombre: "Mar", apellido: "Rivera", descripcion: "Paris" }
+    { id: "3", nombre: "Mar", apellido: "Rivera", descripcion: "Paris" },
+    { id: "4", nombre: "Luis", apellido: "Zapata", descripcion: "Argentina" },
+    { id: "5", nombre: "Valentina", apellido: "Martinez", descripcion: "Fuera de lugar" },
+    { id: "6", nombre: "Marta", apellido: "Quintero", descripcion: "Cristiana" },
+    { id: "7", nombre: "Jose", apellido: "Ruiz", descripcion: "Jugador" },
+    { id: "8", nombre: "Brianna", apellido: "Angulo", descripcion: "Bebe chiquita" },
+    { id: "9", nombre: "Andres", apellido: "Calamaro", descripcion: "Cantante" },
+    { id: "10", nombre: "Amanda", apellido: "Quintana", descripcion: "Vecina" },
+    { id: "11", nombre: "Victoria", apellido: "Torres", descripcion: "Empresaria" },
+    { id: "12", nombre: "Joan", apellido: "Figueroa", descripcion: "Locutor" },
+    { id: "13", nombre: "Antonio", apellido: "Solis", descripcion: "Productor" },
+    { id: "14", nombre: "Vicente", apellido: "Fernandez", descripcion: "Cantante" },
+    { id: "15", nombre: "Maribel", apellido: "Guardia", descripcion: "actriz" },
+    { id: "16", nombre: "Lionel", apellido: "Messi", descripcion: "Futbolista" },
+    { id: "17", nombre: "Linda", apellido: "Caicedo", descripcion: "Futbolista" }, 
+    { id: "18", nombre: "Mariana", apellido: "Pajon", descripcion: "Ciclimos" },
+    { id: "19", nombre: "Kobe", apellido: "Brayant", descripcion: "Baloncestista" },
+    { id: "20", nombre: "Cristiano", apellido: "Ronaldo", descripcion: "Futbolista" }
   ];
 
   const handleBuscar = (inputValue) => {
